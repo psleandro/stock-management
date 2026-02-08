cfa7dc6c202d11ce45d0c51cd84e277651a157ae
Leandro Santos <lsaantos@outlook.com>
2026-02-07 21:22:08 -0300

    refactor: move repositories to a dedicated folder

--- api/src/infrastructure/db/mod.rs
+++ api/src/infrastructure/db/mod.rs
@@ -1,11 +1,7 @@
 pub mod db;
 pub mod models;
-pub mod places_repository;
-pub mod product_stock_repository;
-pub mod products_repository;
+mod repositories;
 pub mod schema;
-pub mod stock_movements_repository;
-pub mod suppliers_repository;
 pub mod transaction;
-pub mod user_repository;
-pub mod workspace_repository;
+
+pub use repositories::*;
